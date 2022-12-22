1a422e9b588f0c4f65e7b2cfd7f8ca08156e794d
aumetros <aum3tros@yandex.ru>
2022-12-22 20:44:38 +0300

    Feat: Реализовать редактирование профиля

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,7 @@
 const profile = document.querySelector('.profile');
-
 const editButton = profile.querySelector('.profile__edit-button');
 
 const popup = document.querySelector('.popup');
-
 const closeButton = popup.querySelector('.popup__close-button');
 
 
@@ -16,5 +14,29 @@ function closePopup() {
 }
 
 editButton.addEventListener('click', openPopup);
-
 closeButton.addEventListener('click', closePopup);
+
+
+let nameProfile = profile.querySelector('.profile__name');
+let aboutProfile = profile.querySelector('.profile__about');
+
+let nameInput = popup.querySelector('.popup__input_type_name');
+let aboutInput = popup.querySelector('.popup__input_type_about');
+
+nameInput.value = nameProfile.innerText;
+aboutInput.value = aboutProfile.innerText;
+
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+
+  nameProfile.textContent = nameInput.value;
+
+  aboutProfile.textContent = aboutInput.value;
+
+  popup.classList.remove('popup_opened');
+}
+
+const submitButton = popup.querySelector('.popup__submit-button');
+
+submitButton.addEventListener('click', handleFormSubmit);
