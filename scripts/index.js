9385e4544db8b30a500b24d2fff0ce8b4ff878a9
aumetros <aum3tros@yandex.ru>
2023-01-14 11:27:00 +0300

    Feature: Добавление карточек из коробки

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+// Выбираем элементы DOM - Кнопки открытия и закрытия редактирования профайла
 const profile = document.querySelector('.profile');
 const editButton = profile.querySelector('.profile__edit-button');
 
@@ -10,21 +11,26 @@ let aboutProfile = profile.querySelector('.profile__about');
 let nameInput = popup.querySelector('.popup__input_type_name');
 let aboutInput = popup.querySelector('.popup__input_type_about');
 
+// Функция открытия попапа
 function openPopup() {
   popup.classList.add('popup_opened');
   nameInput.value = nameProfile.innerText;
   aboutInput.value = aboutProfile.innerText;
 }
 
+// Функция закрытия попапа
 function closePopup() {
   popup.classList.remove('popup_opened');
 }
 
+//Вешаем события на кнопки открытия и закрытия попапа редактирования профайла
 editButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
 
+// Выбираем элемент DOM - форма отправки изменения данных профайла
 let formProfileEdit = popup.querySelector('.popup__form-profile-edit');
 
+// Функция отправки изменений данных профайла
 function handleFormSubmit(evt) {
   evt.preventDefault();
   nameProfile.textContent = nameInput.value;
@@ -32,4 +38,56 @@ function handleFormSubmit(evt) {
   closePopup();
 }
 
+//Вешаем событие на кнопку отправки новых данных профайла
 formProfileEdit.addEventListener('submit', handleFormSubmit);
+
+//Массив карточек "из коробки" при загрузке страницы
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+//Выбираем элементы DOM список карточек и template
+const elementTemplate = document.querySelector('#card').content;
+const elementsList = document.querySelector('.elements');
+
+
+//Перебираем массив карточек и создаем из них элементы списка карточек
+initialCards.forEach(function(card) {
+
+  //Клонируем код карточки
+  const element = elementTemplate.querySelector('.element').cloneNode(true);
+
+  //Наполняем клонированную карточку
+  element.querySelector('.element__foto').src = card.link;
+  element.querySelector('.element__title').textContent = card.name;
+
+  //Добавляем на страницу
+  elementsList.append(element);
+
+});
+
+
+
