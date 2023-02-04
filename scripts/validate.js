080d40efded102ec89984c50a148c16b6a808d95
aumetros <aum3tros@yandex.ru>
2023-02-04 09:15:31 +0300

    Реализована базовая часть валидации формы

--- scripts/validate.js
+++ scripts/validate.js
@@ -5,17 +5,57 @@ const formValidationConfig = {
   inactiveButtonClass: 'popup__button_disabled',
   inputErrorClass: 'popup__input_type_error',
   errorClass: 'popup__error_visible'
-}
-
-const showInputError = (formSelector, inputSelector, errorMessage) => {
-  const errorElement = formSelector.querySelector(`.${inputSelector.id}-error`);
-  errorElement.textContent = errorMessage;
-  errorElement.classList.add('popup-profile__input-error_type_active');
 };
 
-function enableValidation(config) {
-const form = document.querySelector(config.formSelector);
-console.log(form);
+const hideInputError = (errorElement, errorClass) => {
+  errorElement.classList.remove(errorClass);
+  errorElement.textContent = '';
+};
+
+const showInputError = (errorElement, input, errorClass) => {
+  errorElement.classList.add(errorClass);
+  errorElement.textContent = input.validationMessage;
+};
+
+const checkInputValidity = (event, form, config) => {
+   const input = event.target;
+   const inputId = input.id;
+   const errorElement = form.querySelector(`.${inputId}-error`);
+  if (input.validity.valid) {
+    hideInputError(errorElement, config.errorClass);
+  } else {
+    showInputError(errorElement, input, config.errorClass);
+  }
+};
+
+const toggleButtonState = (form, config) => {
+  const buttonElement = form.querySelector(config.submitButtonSelector);
+  const isFormValid = form.checkValidity();
+  buttonElement.disabled = !isFormValid;
+  buttonElement.classList.toggle(config.inactiveButtonClass, !isFormValid);
 }
 
-// enableValidation(formValidationConfig);
+const setEventListeners = (form, config) => {
+  const inputList = Array.from(form.querySelectorAll(config.inputSelector));    
+  inputList.forEach((item) => {
+      item.addEventListener('input', function (event) {
+        checkInputValidity(event, form, config);        
+    });
+  });
+};
+
+
+//Объвляем 
+function enableValidation(config) {
+const form = document.querySelector(config.formSelector);
+
+form.addEventListener('input', () => {
+  toggleButtonState(form, config);
+});
+
+setEventListeners(form, config);
+toggleButtonState(form, config);
+
+}
+
+enableValidation(formValidationConfig);
