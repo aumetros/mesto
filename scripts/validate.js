8c5cf822f7ae7fe448127357a2e7d73e2e588ed6
aumetros <aum3tros@yandex.ru>
2023-02-04 11:51:25 +0300

    Реализовано структурирование кода и комментарии

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,3 +1,4 @@
+//Конфиг селекторов и классов валидации форм
 const formValidationConfig = {
   formSelector: '.popup__form',
   inputSelector: '.popup__input',
@@ -7,19 +8,23 @@ const formValidationConfig = {
   errorClass: 'popup__error_visible'
 };
 
+//Функция скрытия сообщения об ошибке
 const hideInputError = (errorElement, errorClass) => {
   errorElement.classList.remove(errorClass);
   errorElement.textContent = '';
 };
 
+//Функция отображения сообщения об ошибке
 const showInputError = (errorElement, input, errorClass) => {
   errorElement.classList.add(errorClass);
   errorElement.textContent = input.validationMessage;
 };
 
+//Функция проверки валидности формы и отображение ошибки
 const checkInputValidity = (form, inputElement, config) => {
   const inputId = inputElement.id;
   const errorElement = form.querySelector(`.${inputId}-error`);
+
   if (inputElement.validity.valid) {
     hideInputError(errorElement, config.errorClass);
   } else {
@@ -27,15 +32,18 @@ const checkInputValidity = (form, inputElement, config) => {
   }
 };
 
+//Функция состояния кнопки при проверки валидности формы
 const toggleButtonState = (form, config) => {
   const buttonElement = form.querySelector(config.submitButtonSelector);
   const isFormValid = form.checkValidity();
   buttonElement.disabled = !isFormValid;
   buttonElement.classList.toggle(config.inactiveButtonClass, !isFormValid);
-}
+};
 
+//Прописываем каждому полю проверку на валидность
 const setEventListeners = (form, config) => {
   const inputList = Array.from(form.querySelectorAll(config.inputSelector));
+
   inputList.forEach((item) => {
     item.addEventListener('input', function () {
       checkInputValidity(form, item, config);
@@ -44,11 +52,13 @@ const setEventListeners = (form, config) => {
 };
 
 
-//Объвляем функцию валидации
+//Объвляем функцию валидации на каждую форму на странице
 function enableValidation(config) {
 
+  //Ищем все формы с заданным классом
   const formList = Array.from(document.querySelectorAll(config.formSelector));
-
+  
+  //Прописываем каждой форме валидацию полей и переключатель состояния кнопки
   formList.forEach((form) => {
     form.addEventListener('input', () => {
       toggleButtonState(form, config);
@@ -56,11 +66,6 @@ function enableValidation(config) {
     setEventListeners(form, config);
     toggleButtonState(form, config);
   });
-
-  // const form = document.querySelector(config.formSelector);
-
-
-
 }
 
 enableValidation(formValidationConfig);
